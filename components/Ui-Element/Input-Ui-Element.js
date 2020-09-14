e0a98570a2d1b52f8487e4007ae3a8c4c412aef0
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-09-13 22:17:15 -0400

    it was added select component to inputUiElement and it was uninstall axio-react library to isntall axio

--- components/Ui-Element/Input-Ui-Element.js
+++ components/Ui-Element/Input-Ui-Element.js
@@ -8,55 +8,87 @@ const InputUiElement = ({
   name,
   value,
   onChange,
-  customClassName,
+  className,
   size,
   error,
   label,
   helpText,
   autoComplete,
-  style
-}) => {
-  return (
-    <React.Fragment>
-      <Form.Group controlId={controlId} style={style}>
-        {label && <Form.Label>{label}</Form.Label>}
-        <Form.Control
-          type={type}
-          placeholder={placeholder}
-          name={name}
-          value={value}
-          className={customClassName}
-          size={size}
-          onChange={onChange}
-          autoComplete={autoComplete}
-        />
-        {helpText && <Form.Text muted>{helpText}</Form.Text>}
-        <Form.Control.Feedback type={error && "invalid"}>
-          {error}
-        </Form.Control.Feedback>
-      </Form.Group>
-    </React.Fragment>
-  );
-};
+  style,
+  ...props
+}) => (
+  <React.Fragment>
+    <Form.Group controlId={controlId} style={style}>
+      {label && <Form.Label>{label}</Form.Label>}
+      <Form.Control
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        className={className}
+        size={size}
+        onChange={onChange}
+        autoComplete={autoComplete}
+        {...props}/>
+      {helpText && <Form.Text muted>{helpText}</Form.Text>}
+      <Form.Control.Feedback type="invalid">
+        {error}
+      </Form.Control.Feedback>
+    </Form.Group>
+  </React.Fragment>
+);
 
-InputUiElement.defaultProps ={
-  type:"text",
-  customClassName:"",
-  autoComplete:"off"
-}
+InputUiElement.defaultProps = {
+  type: "text",
+  className: "",
+  autoComplete: "off",
+  readOnly:false
+};
 
 InputUiElement.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   type: PropTypes.oneOf(["text", "email", "password", "number"]).isRequired,
-  customClassName: PropTypes.string,
+  className: PropTypes.string,
   value: PropTypes.any,
   onChange: PropTypes.func,
   controlId: PropTypes.string.isRequired,
   size: PropTypes.string,
   style: PropTypes.object,
   helpText: PropTypes.string,
-  autoComplete:PropTypes.string
+  autoComplete: PropTypes.string,
+  readOnly:PropTypes.bool
 };
 
-export default InputUiElement;
+
+const SelectUiElement = ({
+  label,
+  size,
+  controlId,
+  option,
+  style,
+  defaultFirstOption
+}) => (
+  <Form.Group controlId={controlId} style={style}>
+    {label && <Form.Label>{label}</Form.Label>}
+    <Form.Control as="select" size={size}>
+      {defaultFirstOption && <option>{defaultFirstOption}</option>}
+      {option.map((item, index) => (
+        <option key={index} value={item.value}>
+          {item.text}
+        </option>
+      ))}
+    </Form.Control>
+  </Form.Group>
+);
+
+SelectUiElement.propTypes = {
+  label: PropTypes.string,
+  size: PropTypes.string,
+  controlId: PropTypes.string,
+  option: PropTypes.array.isRequired,
+  style: PropTypes.object,
+  defaultFirstOption:PropTypes.string
+};
+
+export { InputUiElement, SelectUiElement };
